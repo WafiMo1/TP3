c4fd9b6612a999ae7019f788a211c9f8fc403dd5
“Wafi <you@example.com>
2021-05-13 14:06:33 -0400

    Ajout url competition

--- baseDonnes.js
+++ baseDonnes.js
@@ -51,4 +51,22 @@ function createNode(element) {
       console.log(JSON.stringify(error));
     }); 
 
-   
+    //COMPÉTITION
+    const club_ul = document.getElementById("compétition");
+  const url = "http://localhost:8080/ords/tp3/compétition";
+  fetch(url)
+  .then((resp) => resp.json())
+  .then(function (data) {
+    //CE SONT DES ALIAS
+    let competitionNum = data.items; //.results;
+    return clubNum.map(function (club) {
+      let ta = createNode("p"),
+      span = createNode("span");
+      span.innerHTML = `Numéro du club: ${club.num_club} `;//ORDRE DÉCIDÉ
+      append(ta, span);
+      append(club_ul, ta);
+});
+  })
+  .catch(function (error) {
+    console.log(JSON.stringify(error));
+  }); 
